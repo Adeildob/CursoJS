014a272cf3065778db1df05015d1d7de6d77f7d1
Adeildo Barros <adeildob@hotmail.com>
2024-06-20 19:24:38 -0300

    Adicionado os exercícios 8, 9 e 10

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 78.5;
+console.log(`A velocidade de seu carro é ${vel}`);
+
+if (vel > 60) {
+  //Condição Simples
+  console.log("Você ultrapassou velocidade permitida. MULTADO!");
+}
+console.log("Dirija sempre usando cinto de segurança!");
